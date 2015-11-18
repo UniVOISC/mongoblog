72f35bf01384e538179c5b51b1135cc264c4b71a
Paulina Elizalde <pelizaldea@gmail.com>
2015-11-18 14:27:35 -0600

    Agregando configuración

--- config.php
+++ config.php
@@ -1,2 +1,10 @@
 <?php
 define('URL', 'http://localhost/mongoblog');
+
+$config = array(
+	'username' => 'user',
+	'password' => 'pass',
+	'dbname'   => 'blog',
+	//'cn' 	   => sprintf('mongodb://%s:%d/%s', $hosts, $port,$database),
+	'connection_string'=> sprintf('mongodb://%s:%d/%s','127.0.0.1','27017','blog')
+);
